3b7fa1c29c09f3c94336af9aaff0790f0d5545c8
dilip1996-git <dilip1996-git@users.noreply.github.com>
2025-08-05 23:43:50 +0530

    Updated settings.py

--- Techblume_Funding/settings.py
+++ Techblume_Funding/settings.py
@@ -31,7 +31,8 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 CORS_ORIGIN_ALLOW_ALL = True
-CSRF_TRUSTED_ORIGINS = ['https://127.0.0.1:8000',]
+CSRF_TRUSTED_ORIGINS = ['https://127.0.0.1:8000',
+                        'https://techblume-funding-917z.onrender.com']
 
 
 
